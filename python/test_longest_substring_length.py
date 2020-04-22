78f2057e6e67e71e6fbe7d88e04d5b36a45ef427
Rogério Libarino Aguilar <rogerio.l.aguilar@gmail.com>
2020-04-22 01:21:16 -0300

    wip: creating function to find the length of the longest substring without repeating characters

---
+++ python/test_longest_substring_length.py
@@ -0,0 +1,33 @@
+import unittest
+from longest_substring_length import LongestSubstringLength
+
+class TestLongestSubstringLength(unittest.TestCase):
+  def setUp(self):
+    self.soluction = LongestSubstringLength()
+  
+  def test_when_string_has_same_chars(self):
+    result = self.soluction.lengthOfLongestSubstring('bbbb')
+    self.assertEqual(result, 1)
+
+  def test_when_all_chars_are_different(self):
+    result = self.soluction.lengthOfLongestSubstring('abcde')
+    self.assertEqual(result, 5)
+
+  def test_when_string_has_two_possibles_substrings(self):
+    result = self.soluction.lengthOfLongestSubstring('abcddcb')
+    self.assertEqual(result, 4)
+    
+  def test_when_the_longest_is_the_second_substring(self):
+    result = self.soluction.lengthOfLongestSubstring('abczzabcde')
+    self.assertEqual(result, 6)
+
+  def test_when_the_longest_substring_is_in_the_middle(self):
+    result = self.soluction.lengthOfLongestSubstring('abczzabcdeesa')
+    self.assertEqual(result, 6)
+
+  def test_when_the_repeated_char_appears_after_others_chars(self): # TODO Failing
+    result = self.soluction.lengthOfLongestSubstring('abcaer')
+    self.assertEqual(result, 4)
+
+if __name__ == '__main__':
+    unittest.main()
